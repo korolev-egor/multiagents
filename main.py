1f9f2c5ac318254850a389fb2677e59ce4a18044
yurnero52 <73389899+yurnero52@users.noreply.github.com>
2021-12-22 17:12:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,108 @@
+from time import sleep
+from asyncio import sleep as async_sleep
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
+from random import randint, random
+from spade.agent import Agent
+from spade import quit_spade
+
+
+ANSWER_FOUND = False
+
+
+class MyAgent(Agent):
+    class MyBehaviour(CyclicBehaviour):
+        def __init__(self, name, neighbours, values):
+            super().__init__()
+            self.name = name
+            self.neighbours = neighbours
+            self.values = values
+
+
+        async def run(self):
+            for neighbor in self.neighbours:
+                msg = Message(to=neighbor)
+                msg.body = str(self.values)
+                await self.send(msg)
+                print(f'{self.name} sent message to {neighbor}')
+            if len(self.values) == len(adj_list):
+                print(f'\nValues: {values}')
+                print(f'Computed mean: {sum(self.values.values()) / len(adj_list)}')
+                print(f'True mean: {sum(values)/5}')
+                print('Topology:\n')
+                for k, v in adj_list.items():
+                    print(k, v)
+                global ANSWER_FOUND
+                ANSWER_FOUND = True
+                raise SystemExit
+            msg = await self.receive()
+            if msg:
+                self.values.update(eval(msg.body))
+
+
+    async def setup(self):
+        self.b = self.MyBehaviour(self.jid, self.neighbours, self.values)
+        self.add_behaviour(self.b)
+        await async_sleep(0.1)
+
+    def __init__(self, name, password, neighbours, value):
+        super().__init__(name, password)
+        self.neighbours = neighbours
+        self.values = {name:value}
+
+
+def generate_list():
+    def try_list():
+        adj_list = dict()
+        for jid in agent_jid:
+            temp = []
+            for i in range(5):
+                if round(random()) == 1 and agent_jid[i] != jid:
+                    temp.append(agent_jid[i])
+            if temp == []:
+                raise Exception
+            adj_list[jid] = temp
+        return adj_list
+    def is_connected():
+        visited, stack = [], [list(adj_list.keys())[0]]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.append(vertex)
+                stack.extend(set(adj_list[vertex]) - set(visited))
+        if len(visited) == len(adj_list):
+            return True
+        return False
+    while True:
+        try:
+            adj_list = try_list()
+            if is_connected():
+                return adj_list
+            else:
+                # print(adj_list)
+                continue
+        except Exception:
+            continue
+            
+
+if __name__ == "__main__":
+    values = [randint(1, 100) for _ in range(5)]
+    agent_jid = ['arst1@01337.io', 'arst2@01337.io', 'arst3@01337.io', 'arst4@01337.io', 'arst5@01337.io']
+    adj_list = generate_list()  # Список смежности
+    agent1 = MyAgent(agent_jid[0], 'arst', adj_list[agent_jid[0]], values[0])
+    agent2 = MyAgent(agent_jid[1], 'arst', adj_list[agent_jid[1]], values[1])
+    agent3 = MyAgent(agent_jid[2], 'arst', adj_list[agent_jid[2]], values[2])
+    agent4 = MyAgent(agent_jid[3], 'arst', adj_list[agent_jid[3]], values[3])
+    agent5 = MyAgent(agent_jid[4], 'arst', adj_list[agent_jid[4]], values[4])
+    agent1.start()
+    agent2.start()
+    agent3.start()
+    agent4.start()
+    agent5.start()
+    while not ANSWER_FOUND:
+        try:
+            sleep(1)
+        except SystemExit:
+            break
+        except KeyboardInterrupt:
+            break
